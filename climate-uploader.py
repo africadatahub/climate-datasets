0b81b368be02076b3b991948aa9960f3706d778f
Mahlatse <mtmolotom@yahoo.com>
2023-04-03 13:34:24 +0200

    changes to script

--- climate-uploader.py
+++ climate-uploader.py
@@ -2,82 +2,115 @@ from dotenv import load_dotenv
 import requests
 import json
 import pandas as pd
-import csv
+import numpy as np
+import argparse
 import os
+from functools import partial
 
 load_dotenv()
 
 # Define CKAN API endpoint and your API key]
+csv_path =os.getenv('filename')
+csv_delimiter = ","
 ckan_api_url = os.getenv('ckan_url')
 api_key =  os.getenv('api_keys')
-resource_id =  os.getenv('resource_id')
+resource_id = os.getenv('resource_id')
 
-# CSV file path and delimiter
-CSV_FILE_PATH =os.getenv('filename')
-CSV_DELIMITER = ","
+def datastore_delete(ckan_api_url, resource_id, api_key):
+    print('Deleting datastore for ' + resource_id)
 
-# Read data from CSV file
-with open(CSV_FILE_PATH) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=CSV_DELIMITER)
-    data = [row for row in reader]
+    u = ckan_api_url + "/api/3/action/datastore_delete"
+    r = requests.post(u, headers={
+      "X-CKAN-API-Key": api_key,
+      "Accept": "application/json",
+      'Content-Type': 'application/json',
+    }, data=json.dumps({
+        "resource_id": resource_id,
+        "force": "true"
+    }))
 
+    return json.loads(r.text)
 
-#create data store
+def datastore_create(records, fields, ckan_api_url, resource_id, api_key):
+    print('Creating new datastore for ' + resource_id)
 
-print('Creating new datastore for ' + resource_id)
+    u = ckan_api_url + "/api/3/action/datastore_create"
+    r = requests.post(u, headers={
+      "X-CKAN-API-Key": api_key,
+      "Accept": "application/json",
+      'Content-Type': 'application/json',
+    }, data=json.dumps({
+        "resource_id": resource_id,
+        "records": records,
+        "fields": fields,
+        "force": "true"
+    }))
+    return json.loads(r.text)
 
-u = ckan_api_url + "/api/action/datastore_create"
-r = requests.post(u, headers={
-    "X-CKAN-API-Key": ckan_api_url,
-    "Accept": "application/json",
-    'Content-Type': 'application/json',
-}, data=json.dumps({
-    "resource_id": resource_id,
-    "force": "true"
-}))
-
-
-# Create API call payload with resource ID and updated fields
-payload = {
-    'id': resource_id,
-    #**new_fields
-}
-
-# Make API call to update dataset - delete the datastore
-headers = {
-    'Authorization': api_key
-}
-response = requests.put(ckan_api_url, headers=headers, json=payload)
-print('Deleting datastore for ' + resource_id)
-u = ckan_api_url+'/api/3/action/datastore_upsert'
-
-response = requests.post(u, headers={
-"X-CKAN-API-Key": api_key,
-"Accept": "application/json",
- 'Content-Type': 'application/json',
-  }, data=json.dumps({
-      "resource_id": resource_id,
-      "force":True
-
-   }))
+def update_resource(df, csv_path, ckan_api_url, resource_id, api_key):
     
-# Update resource data in CKAN
+    df.to_csv(csv_path, index=False)
+    u = ckan_api_url + "/api/3/action/resource_patch"
+    r = requests.post(u, headers={
+      "X-CKAN-API-Key": api_key
+    }, data={
+        "id": resource_id
+    }, files=[('upload', open('./' + csv_path, 'r'))])
+    data_output = json.loads(r.content)
+    print(data_output)
 
-new_data = {
-    "id": resource_id,
-    "month_number": "month_number",
-    "latitude": "latitude",
-    "longitude": "longitude",
-    "time": "time",
-    "climatology": "climatology"
-}
+print('Fetching Data')
 
-headers = {
-    "Authorization": api_key,
-    "Content-Type": "application/json"
-}
-payload = {
-    "resource": new_data
-}
-response = requests.put(ckan_api_url+"/" + "resource_update?id=" + resource_id, headers=headers, json=payload)
-   
+climatology_df = pd.read_csv('TAVG-climatology.csv')
+print(climatology_df)
+
+df = climatology_df
+
+def preparing_climate_data():
+
+    print('Preparing data...')
+    climate_df = pd.DataFrame()
+    records = []
+
+    update_resource(df, 'TAVG-climatology.csv', ckan_api_url,resource_id, api_key)
+
+    for row in climate_df:
+
+        records.append({
+            "month_number": row[0],
+            "latitude": row[1],
+            "longitude": row[2],
+            "time": row[3],
+            "climatology": row[4],
+           
+        })
+
+        fields = [
+            { 
+                "id": "month_number",
+                "type": "numeric"
+            },
+            { 
+                "id": "latitude",
+                "type": "numeric"
+            },
+            { 
+                "id": "longitude",
+                "type": "numeric"
+            },
+            { 
+                "id": "time",
+                "type": "numeric"
+            },
+            { 
+                "id": "climatology",
+                "type": "numeric"
+            }
+            
+        ]
+
+    response = datastore_create(records, fields, ckan_api_url, resource_id, api_key)
+    print(response) 
+
+datastore_delete(ckan_api_url, resource_id, api_key)
+preparing_climate_data()
